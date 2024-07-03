f02f532a83d9426c4eb1d6f81f111901a29d5b38
Dor Kleiman <configurator@gmail.com>
2024-07-03 17:06:03 +0000

    Use json schema locally

--- dist/json-schema-nodejs.js
+++ dist/json-schema-nodejs.js
@@ -2,6 +2,6 @@ import { readFileSync } from "node:fs";
 import { createRequire } from "node:module";
 import { setSchemaDraft07 } from "./json-schema.js";
 const require = createRequire(import.meta.url);
-const filename = require.resolve("ajv/dist/refs/json-schema-draft-07.json");
+const filename = require.resolve("./json-schema-draft-07.json");
 const jsonSchemaSchema = JSON.parse(readFileSync(filename, "utf-8"));
 setSchemaDraft07(jsonSchemaSchema);
