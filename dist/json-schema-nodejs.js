5d49c918b4bac9d2ee4f0c32bf6443f70141b90e
Dor Kleiman <configurator@gmail.com>
2024-07-03 17:14:10 +0000

    read file using require

--- dist/json-schema-nodejs.js
+++ dist/json-schema-nodejs.js
@@ -2,6 +2,5 @@ import { readFileSync } from "node:fs";
 import { createRequire } from "node:module";
 import { setSchemaDraft07 } from "./json-schema.js";
 const require = createRequire(import.meta.url);
-const filename = require.resolve("./json-schema-draft-07.json");
-const jsonSchemaSchema = JSON.parse(readFileSync(filename, "utf-8"));
+const jsonSchemaSchema = require("./json-schema-draft-07.json");
 setSchemaDraft07(jsonSchemaSchema);
