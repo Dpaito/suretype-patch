7c176f26119fad791be527b5ecd65199ffa2236a
Dor Kleiman <configurator@gmail.com>
2024-07-03 17:21:41 +0000

    Simpler import

--- dist/json-schema-nodejs.js
+++ dist/json-schema-nodejs.js
@@ -1,6 +1,3 @@
-import { readFileSync } from "node:fs";
-import { createRequire } from "node:module";
 import { setSchemaDraft07 } from "./json-schema.js";
-const require = createRequire(import.meta.url);
-const jsonSchemaSchema = require("./json-schema-draft-07.json");
+import jsonSchemaSchema from "./json-schema-draft-07.json";
 setSchemaDraft07(jsonSchemaSchema);
